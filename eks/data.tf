ed91768ca53fae6fff6109c2f6e5dc5256f36c69
Samiksha-Kamble39 <samikshakamble115@gmail.com>
2026-02-07 12:08:55 +0530

    Create data.tf

---
+++ eks/data.tf
@@ -0,0 +1,11 @@
+data "aws_subnet" "az2" {
+  id = "subnet-092d1aea8dcfbe6bf"
+}
+
+data "aws_subnet" "az3" {
+  id = "subnet-001430d75f3d503ff"
+}
+
+data "aws_subnet" "az4" {
+  id = "subnet-077528aa906af9421"
+}
