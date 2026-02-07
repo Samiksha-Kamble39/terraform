e2e43c50de462ef9324362a20cd0651757395f3f
Samiksha-Kamble39 <samikshakamble115@gmail.com>
2026-02-07 11:54:14 +0530

    Update main.tf

--- eks/main.tf
+++ eks/main.tf
@@ -1,37 +1,38 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "eu-north-1"
 }
+
 resource "aws_iam_role" "cluster_role" {
-    name = "eks-cluster_role1"
-    assume_role_policy = jsonencode({
+  name = "eks-cluster-role1"
+
+  assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "sts:AssumeRole",
-          "sts:TagSession"
-        ]
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "sts:AssumeRole",
+        "sts:TagSession"
+      ]
+      Principal = {
+        Service = "eks.amazonaws.com"
       }
-    ]
+    }]
   })
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-    role = aws_iam_role.cluster_role.name
+  role       = aws_iam_role.cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
 resource "aws_iam_role" "node_group_role" {
-    name = "node-group-role1"
+  name = "node-group-role1"
 
-    assume_role_policy = jsonencode({
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRole"
       Effect = "Allow"
+      Action = "sts:AssumeRole"
       Principal = {
         Service = "ec2.amazonaws.com"
       }
@@ -40,99 +41,58 @@ resource "aws_iam_role" "node_group_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "worker_node_policy" {
-    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-    role = aws_iam_role.node_group_role.name
+  role       = aws_iam_role.node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "CNI_policy" {
-    policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-    role = aws_iam_role.node_group_role.name
+resource "aws_iam_role_policy_attachment" "cni_policy" {
+  role       = aws_iam_role.node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-resource "aws_iam_role_policy_attachment" "ec2_policy" {
-    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-    role = aws_iam_role.node_group_role.name
+resource "aws_iam_role_policy_attachment" "ecr_policy" {
+  role       = aws_iam_role.node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
-
-resource "aws_vpc" "eks_vpc" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "eks-vpc"
-  }
-}
-
-resource "aws_subnet" "az1" {
-  vpc_id            = aws_vpc.eks_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "eks-subnet-1"
-  }
-}
-
-resource "aws_subnet" "az2" {
-  vpc_id            = aws_vpc.eks_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "eks-subnet-2"
-  }
-}
-
-resource "aws_subnet" "az3" {
-  vpc_id            = aws_vpc.eks_vpc.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-east-1c"
-
-  tags = {
-    Name = "eks-subnet-3"
-  }
-}
-
 
 resource "aws_eks_cluster" "eks_cluster" {
-   name = "eks"
-   version = "1.31"
+  name    = "eks"
+  version = "1.31"
 
-   access_config {
+  role_arn = aws_iam_role.cluster_role.arn
+
+  access_config {
     authentication_mode = "API"
-   }
+  }
 
-   role_arn = aws_iam_role.cluster_role.arn
-   
-
-   vpc_config {
+  vpc_config {
     subnet_ids = [
-      aws_subnet.az1.id,
       aws_subnet.az2.id,
       aws_subnet.az3.id,
+      aws_subnet.az4.id
     ]
-                 
-   }
+  }
 
-   depends_on = [
+  depends_on = [
     aws_iam_role_policy_attachment.eks_cluster_policy
-   ]
+  ]
 }
 
 resource "aws_eks_node_group" "node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
-  node_group_name = "my_node_group"
-  node_role_arn  = aws_iam_role.node_group_role.arn
+  node_group_name = "my-node-group"
+  node_role_arn   = aws_iam_role.node_group_role.arn
 
   subnet_ids = [
-    aws_subnet.az1.id,
     aws_subnet.az2.id,
     aws_subnet.az3.id,
+    aws_subnet.az4.id
   ]
 
   scaling_config {
     desired_size = 1
-    max_size     = 2
     min_size     = 1
+    max_size     = 2
   }
 
   update_config {
@@ -141,7 +101,7 @@ resource "aws_eks_node_group" "node_group" {
 
   depends_on = [
     aws_iam_role_policy_attachment.worker_node_policy,
-    aws_iam_role_policy_attachment.CNI_policy,
-    aws_iam_role_policy_attachment.ec2_policy
+    aws_iam_role_policy_attachment.cni_policy,
+    aws_iam_role_policy_attachment.ecr_policy
   ]
 }
